64830d34e3b3e85ecc7ef8bab3ec82c3d48158d6
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-06-20 08:37:39 -0300

    Add files via upload

---
+++ listaObjetos.js
@@ -0,0 +1,24 @@
+const cliente = {
+    nome:"João",
+    idade: 24,
+    email:"joao@larissa.com",
+    telefone:["(41) 9999-0000", "(41) 9988-0000"],
+};
+
+cliente.edereco = {
+    rua : "R. Joseph Climber",
+    numero : 1337,
+    apartamento : true,
+    complemento : "Ap 934",
+};
+
+cliente.endereco = [
+{
+    rua : "R. Joseph Climber",
+    numero : 1337,
+    apartamento : true,
+    complemento : "Ap 934",
+},
+];
+
+console.log(cliente.endereco);
